24d9cf764b0b66dc160b0b2cde589bec69c54998
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-07 16:38:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ message.channel.send('**Roles Was Succsesfluy Created**')
   }
 });
 client.on('message', message => {
-  if (message.content.startsWith("!readyserver")) {
+  if (message.content.startsWith("-readyserver")) {
   if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
           if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`${message.author.username} You Dont Have ``MANAGE_CHANNELS`` **Premission**`);
    message.guild.createChannel('rules', 'text')
