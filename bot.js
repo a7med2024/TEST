fbcfc0d2f68da43f2de496ffcb665b7a60c388de
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-08 18:21:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,32 +44,6 @@ if (!channel) return;
 channel.send(`** Welcome To Friends server:dove:<@${member.user.id}>** `)  
 }});
 
-var anti_spam = require("discord-anti-spam");
- 
-anti_spam(bot, {
-  warnBuffer: 7,  //MHSTR
-  maxBuffer: 8,  //MHSTR
-  interval: 1000,  //MHSTR
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //MHSTR
-  banMessage: "تم الطرد بسبب السبام",  //MHSTR
-  maxDuplicatesWarning: 7, //NAROX
-  maxDuplicatesBan: 10  //NAROX
-});
- 
-bot.login(process.env.BOT_SPAM);
- 
-const bot1 = new Discord.Client();
- 
- var anti_spam1 = require("discord-anti-spam");
-anti_spam1(bot1, {
-  warnBuffer: 7,  //@N.C™ » MHSTR#1119  //// حقوق مهستر
-  maxBuffer: 8,  //@N.C™ » MHSTR#1119
-  interval: 1000, //@N.C™ » MHSTR#1119
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //NAROX codes
-  banMessage: "تم الطرد بسبب السبام",  //NAROX codes
-  maxDuplicatesWarning: 7,  //NAROX codes
-  maxDuplicatesBan: 10  //MHSTR
-});
 
 
 client.on("ready", async  => {
