a8519efb5d4df04ff5597debfc8a0287606880e8
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-08 16:58:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,16 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
+
+client.on('message', message => {
     if (message.content === '!help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**برفكس البوت(!)**')
