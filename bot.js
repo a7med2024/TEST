6767a3484947e8cea7274382449eea60d30dde95
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-10 16:24:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1524,7 +1524,7 @@ const invites = {};
 const wait = require('util').promisify(setTimeout);
 
 client.on('ready', () => {
-  wait(2000);
+  wait(5000);
 
   client.guilds.forEach(g => {
     g.fetchInvites().then(guildInvites => {
