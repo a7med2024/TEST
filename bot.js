f1f16886af17b111dc9e54a774fb92ba7b908994
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-12 19:51:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2093,17 +2093,6 @@ client.on('message', message => {
     });
    
 
-client.on('message', async message => {
-    
-    let args = message.content.split(' ').slice(1);
-if (message.content.startsWith("!dcolor")) {
-if(!message.member.hasPermission('ADMINISTRATOR')) return
-let role = message.guild.roles.find('name', 'رقم الون');
-
-role.delete()
-}
-
-});
 
 client.on('message', message => {
 	 var prefix = "!";
