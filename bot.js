a7695eda7263fca25be2e76aa8ec13d3a7887b33
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-12 14:03:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const request = require('request');
 const jimp = require('jimp');
 const Canvas = require('canvas');
 const fs = require("fs");
+const weather = require('weather-js');
 const prefix = '!'
 const devs = ["514041065041297421"] 
 const adminprefix = "!";
@@ -2647,6 +2648,44 @@ client.on('message', function(message) {
     }
 })
 
+client.on('message', message => {
+    let msg = message.content.toUpperCase(); 
+    let cont = message.content.slice(prefix.length).split(" "); 
+    let args = cont.slice(1); 
+    if (msg.startsWith(prefix + 'weather')) { 
+
+        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
+            if (err) message.channel.send(err);
+
+            
+            if (result.length === 0) {
+                message.channel.send('**Please enter a valid location.**').
+                return; 
+            }
+
+           
+            var current = result[0].current; 
+            var location = result[0].location; 
+
+           
+            const embed = new Discord.RichEmbed()
+.setDescription(`**${current.skytext}**`) 
+                .setAuthor(`Weather for ${current.observationpoint}`) 
+                .setThumbnail(current.imageUrl) 
+                .setColor(0x00AE86) 
+                .addField('Timezone',`UTC${location.timezone}`, true) 
+                .addField('Degree Type',location.degreetype, true)
+                .addField('Temperature',`${current.temperature} Degrees`, true)
+                .addField('Feels Like', `${current.feelslike} Degrees`, true)
+                .addField('Winds',current.winddisplay, true)
+                .addField('Humidity', `${current.humidity}%`, true)
+
+                
+                message.channel.send({embed});
+        });
+    }
+
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
