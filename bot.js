454d4d247feb37af2f66785f8233b8ef8bdeea7a
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-19 13:17:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1664,7 +1664,7 @@ client.on('message', message => {
 
 client.on('message', message => {
             let args = message.content.split(' ').slice(1);
-            if(message.content.split(' ')[0] == `${prefix}لون`){
+            if(message.content.split(' ')[0] == `لون`){
             const embedd = new Discord.RichEmbed()
             .setFooter('Requested by '+message.author.username, message.author.avatarURL)
             .setDescription(`**لا يوجد لون بهذا الأسم ** ❌ `)
