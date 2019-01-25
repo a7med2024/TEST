f188da037293d7c443c7348ad89a65a8c783b38f
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2019-01-25 14:36:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3515,27 +3515,6 @@ client.on('message', message => {
  
   });
 
-client.on('message', function(message) {
-   if (!message.guild.member(message.author).hasPermission(8)) return;
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | تمت معاقبتك")
-            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-      .addField(`by`,`A7med`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL) 
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500) 
-    
-        
-    }
-    
-})
 //This Bot by a7med//This Bot by a7med//This Bot by a7med
 //This Bot by a7med//This Bot by a7med//This Bot by a7med
 //This Bot by a7med//This Bot by a7med//This Bot by a7med
